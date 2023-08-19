a7d0d35963679d0274cd2ce5760ced0869171257
Moonkey <moonkey48@icloud.com>
2023-08-19 11:52:13 +0900

    feat - add STT example

---
+++ JunctionAsia2023-GAGA/JunctionAsia2023-GAGA/Source/View/STTTestView.swift
@@ -0,0 +1,41 @@
+//
+//  STTTestView.swift
+//  JunctionAsia2023-GAGA
+//
+//  Created by Seungui Moon on 2023/08/19.
+//
+
+import SwiftUI
+
+struct STTTestView: View {
+    
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
+    
+    var body: some View {
+        VStack {
+            Text(speechRecognizer.transcript)
+            Button {
+                speechRecognizer.stopTranscribing()
+            } label: {
+                Text("stop")
+            }
+            Button {
+                speechRecognizer.resetTranscript()
+            } label: {
+                Text("reset")
+            }
+            Button {
+                speechRecognizer.startTranscribing()
+            } label: {
+                Text("start")
+            }
+        }
+    }
+}
+
+struct STTTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        STTTestView()
+    }
+}
