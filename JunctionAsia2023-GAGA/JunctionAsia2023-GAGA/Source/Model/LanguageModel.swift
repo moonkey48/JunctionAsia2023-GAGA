df483c1e374251b006643b49042c93456b6d2886
Damagucci-Juice <jsr9874@gmail.com>
2023-08-19 14:39:07 +0900

    feat: Papago 번역 모델 구현

--- JunctionAsia2023-GAGA/JunctionAsia2023-GAGA/Source/Model/LanguageModel.swift
+++ JunctionAsia2023-GAGA/JunctionAsia2023-GAGA/Source/Model/LanguageModel.swift
@@ -13,6 +13,8 @@ enum TranslationError: Error {
     case targetEmpty
     case contentEmpty
     case invalidResponse
+    case decodingError
+    case secretePlistError
 }
 
 final class LanguageModel: ObservableObject {
@@ -21,29 +23,58 @@ final class LanguageModel: ObservableObject {
     // 운전자 언어
     private let targetLangType: String = "ko"
     
+    private var id: String? {
+        guard let url = Bundle.main.url(forResource: "PapagoAPI", withExtension: "plist") else { return nil }
+        guard let dictionary = try? NSDictionary(contentsOf: url, error: ()) else { return nil }
+        return dictionary[String.id] as? String
+    }
     
-    func fetchTranslation(from source: String, to target: String,  with text: String) async throws -> String {
+    private var secrete: String? {
+        guard let url = Bundle.main.url(forResource: "PapagoAPI", withExtension: "plist") else { return "" }
+        guard let dictionary = try? NSDictionary(contentsOf: url, error: ()) else { return "" }
+        return dictionary[String.secrete] as? String
+    }
+    
+    @Published var translatedText = "번역하기 전 말이 여기 있습니다."
+    
+    // fetch translation
+    func fetchTranslation(with text: String) async throws {
+        // Info.plist settup
+        guard let id, let secrete else { throw TranslationError.secretePlistError }
+        
         // 요청 URL
         guard let apiUrl = URL(string: "https://openapi.naver.com/v1/papago/n2mt") else { throw TranslationError.invalidURL }
         
         // 요청 데이터 설정
-        let requestBody = "source=\(source)&target=\(target)&text=\(text)"
+        let requestBody = "source=\(sourceLangType)&target=\(targetLangType)&text=\(text)"
         let requestData = requestBody.data(using: .utf8)
         
         // 요청 헤더 설정
         var request = URLRequest(url: apiUrl)
         request.httpMethod = "POST"
         request.addValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        request.addValue("{애플리케이션 등록 시 발급받은 클라이언트 아이디 값}", forHTTPHeaderField: "X-Naver-Client-Id")
-        request.addValue("{애플리케이션 등록 시 발급받은 클라이언트 시크릿 값}", forHTTPHeaderField: "X-Naver-Client-Secret")
+        request.addValue("\(id)", forHTTPHeaderField: String.id)
+        request.addValue("\(secrete)", forHTTPHeaderField: String.secrete)
+        request.httpBody = requestData
 
-
+        // Rest API 요청
         let (data, response) = try await URLSession.shared.data(for: request)
-        guard let httpResponse = response as? HTTPURLResponse,
-              200..<300 ~= httpResponse.statusCode  else { throw TranslationError.invalidResponse }
         
-        return ""
+        // 데이터 정확도
+        guard let httpResponse = response as? HTTPURLResponse,
+              200..<300 ~= httpResponse.statusCode  else {
+            print(response)
+            throw TranslationError.invalidResponse
+        }
+        guard let papagoReseponse = try? JSONDecoder().decode(PapagoResponse.self, from: data) else {
+            throw TranslationError.decodingError }
+        DispatchQueue.main.async { [unowned self] in
+            self.translatedText = papagoReseponse.message.result.translatedText
+        }
     }
 }
 
-// Info.plist에 시크릿 담기
+private extension String {
+    static let id = "X-Naver-Client-Id"
+    static let secrete = "X-Naver-Client-Secret"
+}
