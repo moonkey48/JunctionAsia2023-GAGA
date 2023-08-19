395a97efedeadcaf4cd2474791dd27ff382105ca
Moonkey <moonkey48@icloud.com>
2023-08-20 05:09:53 +0900

    [feat] - make main layout

--- JunctionAsia2023-GAGA/JunctionAsia2023-GAGA/Source/View/MainView.swift
+++ JunctionAsia2023-GAGA/JunctionAsia2023-GAGA/Source/View/MainView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var showTTSModal = false
+    @State private var showSTTModal = false
+    @State private var isTextReceived = false
+    
     var body: some View {
         VStack {
             HStack {
@@ -16,46 +20,89 @@ struct MainView: View {
                 Image(systemName: "gearshape.fill")
                     .font(.system(size: 24))
                     .foregroundColor(Color(hex: "646F7C"))
-
             }
+            .padding()
             
-            VStack(spacing: 16) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 16)
-                        .frame(width: 358, height: 254)
-                        .foregroundColor(Color(hex: "6E65F4"))
-                    Text("📝 Text")
-                        .foregroundColor(Color.white)
-                        .font(.system(.title))
-                    ZStack {
-                        Circle()
-                            .frame(width: 56)
-                            .foregroundColor(Color.white)
-                            .opacity(0.24)
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 24))
-                            .foregroundColor(Color(.white))
+            VStack(spacing: 16)  {
+                TTSComponentView
+                    .onTapGesture {
+                        showTTSModal = true
                     }
-                }
-                ZStack {
-                    RoundedRectangle(cornerRadius: 16)
-                        .frame(width: 358, height: 254)
-                        .foregroundColor(Color(hex: "OB033F"))
-                    Text("🎙️ Voice")
-                        .foregroundColor(Color.white)
-                        .font(.system(.title))
-                    ZStack {
-                        Circle()
-                            .frame(width: 56)
-                            .foregroundColor(Color.white)
-                            .opacity(0.24)
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 24))
-                            .foregroundColor(Color(.white))
+                STTComponentView
+                    .onTapGesture {
+                        showSTTModal = true
                     }
+            }
+            Spacer()
+        }
+        .sheet(isPresented: $isTextReceived, content: {
+            ReceivedTextView()
+        })
+        .sheet(isPresented: $showSTTModal) {
+            STTView()
+        }
+        .sheet(isPresented: $showTTSModal) {
+            TTSView()
+        }
+       
+        
+    }
+}
+
+extension MainView {
+    var TTSComponentView: some View {
+        VStack {
+            HStack{
+                Text("📝 텍스트")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 32, weight: .bold))
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                ZStack {
+                    Circle()
+                        .frame(width: 56)
+                        .foregroundColor(Color.white)
+                        .opacity(0.24)
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 24))
+                        .foregroundColor(Color(.white))
                 }
             }
         }
+        .padding(30)
+        .frame(width: 358, height: 254)
+        .background(Color(hex: "6E65F4"))
+        .cornerRadius(16)
+    }
+    var STTComponentView: some View {
+        VStack {
+            HStack{
+                Text("🎙️ 음성인식")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 32, weight: .bold))
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                ZStack {
+                    Circle()
+                        .frame(width: 56)
+                        .foregroundColor(Color.white)
+                        .opacity(0.24)
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 24))
+                        .foregroundColor(Color(.white))
+                }
+            }
+        }
+        .padding(30)
+        .frame(width: 358, height: 254)
+        .background(Color(hex: "0B033F"))
+        .cornerRadius(16)
     }
 }
 
---
+++ JunctionAsia2023-GAGA/JunctionAsia2023-GAGA/Source/View/ReceivedTextView.swift
@@ -0,0 +1,22 @@
+//
+//  ReceivedTextView.swift
+//  JunctionAsia2023-GAGA
+//
+//  Created by Seungui Moon on 2023/08/20.
+//
+
+import SwiftUI
+
+struct ReceivedTextView: View {
+    var body: some View {
+        VStack {
+            Text("hi")
+        }
+    }
+}
+
+struct ReceivedTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReceivedTextView()
+    }
+}
