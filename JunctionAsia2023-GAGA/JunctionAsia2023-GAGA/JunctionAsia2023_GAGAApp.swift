f29f65b1fa2931add1d77805ccb8bea098d7ae15
Moonkey <moonkey48@icloud.com>
2023-08-19 10:51:34 +0900

    setting - create setting

---
+++ JunctionAsia2023-GAGA/JunctionAsia2023-GAGA/JunctionAsia2023_GAGAApp.swift
@@ -0,0 +1,20 @@
+//
+//  JunctionAsia2023_GAGAApp.swift
+//  JunctionAsia2023-GAGA
+//
+//  Created by Seungui Moon on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct JunctionAsia2023_GAGAApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# GAGA
+
+restart hecking
